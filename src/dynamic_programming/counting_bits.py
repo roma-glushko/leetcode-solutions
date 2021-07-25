6f5a6945eff509317d952c8f66cab94a4cb6e131
Roman Glushko <roman.glushko.m@gmail.com>
2021-07-25 11:33:23 +0300

    Added a solution for Counting Bits problem

---
+++ src/dynamic_programming/counting_bits.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class CountingBits:
+    """
+    Problem Link: https://leetcode.com/problems/counting-bits/
+    Complexity: Easy
+    """
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+
+        bits_number: List[int] = [0, 1]
+
+        if n == 1:
+            return bits_number
+
+        next_two_power: int = 2
+        count_index: int = 0
+
+        for number in range(2, n + 1):
+            if number == next_two_power:
+                # we should reset count index at each number that is power of 2
+                next_two_power *= 2
+                count_index = 0
+
+            bits_number.append(1 + bits_number[count_index])
+            count_index += 1
+
+        return bits_number
---
+++ src/dynamic_programming/counting_bits_test.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+from .counting_bits import CountingBits
+
+
+class CountingBitsTest(TestCase):
+
+    def test_default_inputs(self):
+        solution = CountingBits()
+
+        bit_array = solution.countBits(2)
+        self.assertEqual(bit_array, [0, 1, 1])
+
+        bit_array = solution.countBits(3)
+        self.assertEqual(bit_array, [0, 1, 1, 2])
+
+        bit_array = solution.countBits(4)
+        self.assertEqual(bit_array, [0, 1, 1, 2, 1])
+
+        bit_array = solution.countBits(5)
+        self.assertEqual(bit_array, [0, 1, 1, 2, 1, 2])
+
+    def test_base_cases(self):
+        solution = CountingBits()
+
+        bit_array = solution.countBits(0)
+        self.assertEqual(bit_array, [0])
+
+        bit_array = solution.countBits(1)
+        self.assertEqual(bit_array, [0, 1])
